3d0b9c20267f12dd8b1b4a55978daf6621c88e2b
Anton Barysau <antonborisoff@mail.ru>
2024-03-04 13:34:27 +0300

    inElement wrapper
    Introduce inElement wrapper and make elementText support it;

--- src/app/tests/foundation/tests/test.component.harness.ts
+++ src/app/tests/foundation/tests/test.component.harness.ts
@@ -9,7 +9,7 @@ export class TestComponentHarness extends BaseHarness {
   // not needed in base harness itself;
   // should not be moved to base harness;
   public async elementPresent(id: string, tag: string): Promise<boolean> {
-    const element = await this.locatorForOptional(`${tag}${this.getIdSelector(id)}`)()
+    const element = await this.locatorForOptional(`${this.ancestorSelector}${tag}${this.getIdSelector(id)}`)()
     return !!element
   }
 }
