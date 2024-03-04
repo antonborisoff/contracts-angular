a1cdebee9f64515d7f72aa468c0fb626b326fc2a
antonborisoff <antonborisoff@mail.ru>
2024-03-04 14:08:27 +0300

    contracts component (#43)

---
+++ src/app/components/contracts/tests/contracts.component.harness.ts
@@ -0,0 +1,7 @@
+import {
+  BaseHarness
+} from '../../../tests/foundation/base.component.harness'
+
+export class ContractsHarness extends BaseHarness {
+  public static hostSelector = 'app-contracts'
+}
