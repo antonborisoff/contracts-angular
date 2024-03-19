074d63a5bb9c86b01df59020aeeb597339b4c821
antonborisoff <antonborisoff@mail.ru>
2024-03-19 15:25:52 +0300

    fix type extension and gitignore (#127)

--- e2e/types/wdio.d.ts
+++ e2e/types/wdio.d.ts
@@ -1,3 +1,10 @@
+// import required for proper type extension, but ts lint complains, so we switch it off for import
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Browser
+} from '@wdio/globals/types'
+/* eslint-enable @typescript-eslint/no-unused-vars */
+
 declare global {
   namespace WebdriverIO {
     interface Browser {
