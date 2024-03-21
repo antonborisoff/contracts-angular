906fcede913a6f16f30e3a28631e79ef7e9c8b36
antonborisoff <antonborisoff@mail.ru>
2024-03-21 16:05:30 +0300

    waiting base harness for e2e tests (#132)

--- src/app/tests/foundation/tests/test.component.harness.ts
+++ src/app/tests/foundation/tests/test.component.harness.ts
@@ -1,6 +1,9 @@
 import {
   BaseHarness
 } from '../base.component.harness'
+import {
+  WaitingHarness
+} from '../waiting.component.harness'
 
 export class TestComponentHarness extends BaseHarness {
   public static hostSelector = 'app-test-component'
@@ -13,7 +16,8 @@ export class TestComponentHarness extends BaseHarness {
     return !!element
   }
 
-  // only required for testing waitForElementNotBusy to check that it indeed waits for element becoming not busy;
+  // only required for testing waitForElementNotBusy to check that it indeed waits for element becoming not busy,
+  // thus must not wait for element to become free;
   // not needed in base harness itself;
   // should not be moved to base harness;
   public async elementBusy(id: string): Promise<boolean> {
@@ -21,4 +25,8 @@ export class TestComponentHarness extends BaseHarness {
     const busyState = await element.getAttribute('data-busy')
     return busyState === 'true' || busyState === null
   }
+}
+
+export class WaitingTestComponentHarness extends WaitingHarness {
+  public static hostSelector = 'app-test-component'
 }
