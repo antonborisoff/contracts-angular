ab1a9e9f7d839a2a4f74ed9a587d02377f2160a2
antonborisoff <antonborisoff@mail.ru>
2024-03-22 11:29:55 +0300

    refactor waiting harness (#134)
    1. replace 'waitFor' prefix with 'expect';
    2. Prepare waitFor for reuse for actions;
    3. unsubscribe from timeout to stabilize and isolate tests;

--- src/app/tests/foundation/waiting.component.harness.ts
+++ src/app/tests/foundation/waiting.component.harness.ts
@@ -1,4 +1,8 @@
 import {
+  ComponentHarness,
+  TestElement
+} from '@angular/cdk/testing'
+import {
   BaseHarness
 } from './base.component.harness'
 
@@ -19,15 +23,24 @@ export class WaitingHarness extends BaseHarness {
   protected waitForTimeoutInterval = 15000
   protected waitForPollingInterval = 400
 
-  protected async waitFor(condition: () => Promise<boolean>, errorMessage: string): Promise<void> {
-    let conditionFulfilled = await condition()
+  protected async waitFor(
+    lookup: () => Promise<TestElement | ComponentHarness | boolean>,
+    errorMessage: string,
+    action?: (result: TestElement | ComponentHarness | boolean) => Promise<void>
+  ): Promise<void> {
+    let result = await lookup()
     const endTime = Date.now() + this.waitForTimeoutInterval
-    while (!conditionFulfilled && Date.now() < endTime) {
+    while (!result && Date.now() < endTime) {
       await wait(this.waitForPollingInterval)
-      conditionFulfilled = await condition()
+      result = await lookup()
     }
-    if (!conditionFulfilled) {
+    if (!result) {
       throw new Error(errorMessage)
+    }
+    else {
+      if (action) {
+        action(result)
+      }
     }
   }
 
@@ -46,20 +59,15 @@ export class WaitingHarness extends BaseHarness {
   }
 
   // for elements blocked with appBusy directive
-  public async waitForElementFree(id: string): Promise<void> {
-    const condition = async (): Promise<boolean> => {
+  public async expectElementFree(id: string): Promise<void> {
+    await this.waitFor(async () => {
       const cssSelector = this.getCssSelector(id, ['div'], this.ancestorSelector, ':not([data-busy="true"])')
       return !!(await this.locatorForOptional(cssSelector)())
-    }
-    await this.waitFor(condition, `Waiting for element ${id} becoming free failed: timeout exceeded, but element is still busy.`)
+    }, `Waiting for element ${id} becoming free failed: timeout exceeded, but element is still busy.`)
   }
 
   // for elements hidden via @if + async
-  public async waitForElementPresent(id: string, present: boolean = true): Promise<void> {
-    const condition = async (): Promise<boolean> => {
-      const cssSelector = this.getCssSelector(id, ['div'], this.ancestorSelector)
-      return !!(await this.locatorForOptional(cssSelector)()) === present
-    }
+  public async expectElementPresent(id: string, present: boolean = true): Promise<void> {
     let errorMessage: string
     if (present) {
       errorMessage = `Waiting for element ${id} being present failed: timeout exceeded, but element is still not present.`
@@ -67,6 +75,9 @@ export class WaitingHarness extends BaseHarness {
     else {
       errorMessage = `Waiting for element ${id} not being present failed: timeout exceeded, but element is still present.`
     }
-    await this.waitFor(condition, errorMessage)
+    await this.waitFor(async () => {
+      const cssSelector = this.getCssSelector(id, ['div'], this.ancestorSelector)
+      return !!(await this.locatorForOptional(cssSelector)()) === present
+    }, errorMessage)
   }
 }
